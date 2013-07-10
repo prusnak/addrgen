5ad31479c0553b49a5357afe9e5f6dc1605d4948
Pavol Rusnak <stick@gk2.sk>
2013-07-11 00:00:06 +0200

    make it obvious that python/php examples use the same mpk all the time

--- php/test.php
+++ php/test.php
@@ -3,12 +3,14 @@
 
 require 'addrgen.php';
 
+$mpk = '675b7041a347223984750fe3ab229df0c9f960e7ec98226b7182a2cb1990e39901feecf5a670f1d788ab29f626e20de424f049d216fc6f4c6ec42506763fa28e';
+
 for ($i = 0; $i < 10; $i++) {
-	print addr_from_mpk('675b7041a347223984750fe3ab229df0c9f960e7ec98226b7182a2cb1990e39901feecf5a670f1d788ab29f626e20de424f049d216fc6f4c6ec42506763fa28e', $i) . "\n";
+	print addr_from_mpk($mpk, $i) . "\n";
 }
 
 foreach (array(100, 65537, 4294967296) as $i) {
-	print addr_from_mpk('675b7041a347223984750fe3ab229df0c9f960e7ec98226b7182a2cb1990e39901feecf5a670f1d788ab29f626e20de424f049d216fc6f4c6ec42506763fa28e', $i) . "\n";
+	print addr_from_mpk($mpk, $i) . "\n";
 }
 
 /*
--- python/test.py
+++ python/test.py
@@ -2,10 +2,12 @@
 
 from addrgen import addr_from_mpk
 
+mpk = '675b7041a347223984750fe3ab229df0c9f960e7ec98226b7182a2cb1990e39901feecf5a670f1d788ab29f626e20de424f049d216fc6f4c6ec42506763fa28e'
+
 for i in range(10):
-	print addr_from_mpk('675b7041a347223984750fe3ab229df0c9f960e7ec98226b7182a2cb1990e39901feecf5a670f1d788ab29f626e20de424f049d216fc6f4c6ec42506763fa28e', i)
+	print addr_from_mpk(mpk, i)
 for i in [100, 65537, 4294967296]:
-	print addr_from_mpk('675b7041a347223984750fe3ab229df0c9f960e7ec98226b7182a2cb1990e39901feecf5a670f1d788ab29f626e20de424f049d216fc6f4c6ec42506763fa28e', i)
+	print addr_from_mpk(mpk, i)
 
 # should print:
 #
